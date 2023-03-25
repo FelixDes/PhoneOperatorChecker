3834f548f5534f5508be2dc395e21dde30abda09
felix <vip.zatebe@mail.ru>
2023-03-25 20:36:20 +0300

    Implement the phone controller

--- backend/controllers/PhoneControllers.py
+++ backend/controllers/PhoneControllers.py
@@ -1,12 +1,16 @@
 from dependency_injector.wiring import inject, Provide
-from flask import request
+from flask import request, Response
 
 from backend.MainContainer import MainContainer
+from backend.exceptions.ServiceException import ServiceException
 from backend.services.PhoneRepository import PhoneRepository
 
 
 @inject
 def index(phone_rep: PhoneRepository = Provide[MainContainer.phone_repository]):
     phone_number = request.args.get("phoneNumber", default="", type=str)
-    print(phone_rep.pg_pool)
-    return phone_rep.get_provider(phone_number)
+    try:
+        res = phone_rep.get_provider(phone_number)
+        return Response(response=res, status=200)
+    except ServiceException:
+        return Response(status=400)
