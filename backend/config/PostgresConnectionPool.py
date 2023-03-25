ca5f92cb9cd3e0347b99a718d91e16d41e4b74dc
felix <vip.zatebe@mail.ru>
2023-03-25 20:35:06 +0300

    Modify PostgresConnectionPool for connection putting

--- backend/config/PostgresConnectionPool.py
+++ backend/config/PostgresConnectionPool.py
@@ -3,16 +3,19 @@ from psycopg2 import pool
 
 
 class PostgresConnectionPool:
-    pg_pool = None
+    _pg_pool = None
 
     def __init__(self, pg_config) -> None:
-        self.pg_pool = psycopg2.pool.SimpleConnectionPool(1, 20, user=pg_config["user"],
-                                                          password=pg_config["password"],
-                                                          host=pg_config["host"],
-                                                          port=pg_config["port"],
-                                                          database=pg_config["db"])
-        if self.pg_pool:
+        self._pg_pool = psycopg2.pool.SimpleConnectionPool(1, 20, user=pg_config["user"],
+                                                           password=pg_config["password"],
+                                                           host=pg_config["host"],
+                                                           port=pg_config["port"],
+                                                           database=pg_config["db"])
+        if self._pg_pool:
             print("Connection pool created successfully")
 
     def get_connection(self):
-        return self.pg_pool.getconn()
+        return self._pg_pool.getconn()
+
+    def put_connection(self, conn):
+        return self._pg_pool.putconn(conn)
