80c58d82b996130a8a25185e61ade587bb4830af
felix <vip.zatebe@mail.ru>
2023-03-25 20:36:08 +0300

    Implement the phone repository

--- backend/services/PhoneRepository.py
+++ backend/services/PhoneRepository.py
@@ -1,10 +1,22 @@
 from backend.config.PostgresConnectionPool import PostgresConnectionPool
+from backend.exceptions.ServiceException import ServiceException
 
 
 class PhoneRepository:
 
     def __init__(self, pg_pool: PostgresConnectionPool) -> None:
-        self.pg_pool = pg_pool
+        self._pg_pool = pg_pool
 
     def get_provider(self, phone_number) -> str:
-        return phone_number
+        try:
+            conn = self._pg_pool.get_connection()
+            cur = conn.cursor()
+
+            cur.execute("select operatorName from MovedPhoneNumbers where phonenumber = " + phone_number)
+            rs = cur.fetchone()
+
+            cur.close()
+            self._pg_pool.put_connection(conn)
+            return rs[0]
+        except TypeError:
+            raise ServiceException("Something went wrong during getting the operator name for " + phone_number)
