f01e097ad9ce00d3f7563987053757349b62e351
Felix <vip.zatebe@mail.ru>
2023-04-04 13:54:36 +0300

    Add gunicorn

--- backend/app.py
+++ backend/app.py
@@ -2,22 +2,17 @@ import os
 import secrets
 
 from flask import Flask
-from flask_bootstrap import Bootstrap
 
 import PhoneControllers
 from MainContainer import MainContainer
 
+container = MainContainer()
 
-def create_app():
-    container = MainContainer()
-
-    app = Flask(__name__)
-    app.container = container
-    app.config['SECRET_KEY'] = secrets.token_urlsafe(16)
-    app.add_url_rule("/", "index", PhoneControllers.index)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-
+app = Flask(__name__)
+app.container = container
+app.config['SECRET_KEY'] = secrets.token_urlsafe(16)
+app.add_url_rule("/", "index", PhoneControllers.index)
 
 if __name__ == "__main__":
-    create_app()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
