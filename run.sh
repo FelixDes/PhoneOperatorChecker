c40da357bd6b0ee7b934a08ecbb96068c6d22679
Felix <vip.zatebe@mail.ru>
2023-04-30 16:40:14 +0300

    Add NginX

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
-docker network create backend-internal
-
+docker volume prune -f
 docker-compose down --remove-orphans
 docker-compose rm -f
-docker-compose pull
+#docker-compose pull
 docker-compose up --build -d
