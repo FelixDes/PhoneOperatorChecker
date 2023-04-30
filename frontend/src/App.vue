8ba0cb36727594d8750958e90c93d71551ad9020
Felix <vip.zatebe@mail.ru>
2023-04-30 16:42:34 +0300

    Add request templates

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,35 @@
 <template>
-  <v-app>
-    <v-main>
-      <PhoneChecker />
-    </v-main>
-  </v-app>
+    <v-app>
+        <v-main>
+            <PhoneChecker/>
+        </v-main>
+    </v-app>
 </template>
 
-<script setup>
-  import PhoneChecker from "@/components/PhoneChecker.vue";
+<script>
+import PhoneChecker from "@/components/PhoneChecker.vue";
+import axios from "axios";
+
+export default {
+    name: "App",
+    components: {PhoneChecker},
+
+    mounted() {
+        this.$root.doGet = this.doGet
+    },
+
+    methods: {
+        doGet(url, config) {
+            return this._callRest(axios.get, url, config)
+        },
+        _callRest(method, url, payload, opt) {
+            return new Promise((resolve, reject) => method("api" + url, payload, opt).then(res => {
+                resolve(res.data)
+            }).catch(e => {
+                reject(e)
+            }))
+        },
+    },
+}
+
 </script>
